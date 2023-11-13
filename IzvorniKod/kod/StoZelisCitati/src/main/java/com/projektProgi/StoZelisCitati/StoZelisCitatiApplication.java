d3bb9d3794ca638380405179ff2258bb61e20828
Zela0912 <141058666+Zela0912@users.noreply.github.com>
2023-11-13 14:26:08 +0100

    Add files via upload
    Pokrenut Maven projekt koji ce za backend koristiti spring boot te se spajati na server preko apache tomcata
    Napravljen dio predloska za pocetnu stranicu (FrontEnd)

---
+++ IzvorniKod/kod/StoZelisCitati/src/main/java/com/projektProgi/StoZelisCitati/StoZelisCitatiApplication.java
@@ -0,0 +1,13 @@
+package com.projektProgi.StoZelisCitati;
+
+import    org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoZelisCitatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoZelisCitatiApplication.class, args);
+	}
+
+}
---
+++ IzvorniKod/kod/StoZelisCitati/src/test/java/com/projektProgi/StoZelisCitati/StoZelisCitatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projektProgi.StoZelisCitati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoZelisCitatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
