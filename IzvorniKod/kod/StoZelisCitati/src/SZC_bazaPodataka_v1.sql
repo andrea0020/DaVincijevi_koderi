d39f79500f5aaf09da0bcb4d7f9dae9716d15d01
Edhnil <148586961+Edhnil@users.noreply.github.com>
2023-11-14 13:07:06 +0100

    Update SZC_bazaPodataka_v1.sql

--- IzvorniKod/kod/StoZelisCitati/src/SZC_bazaPodataka_v1.sql
+++ IzvorniKod/kod/StoZelisCitati/src/SZC_bazaPodataka_v1.sql
@@ -30,11 +30,12 @@ create table Knjiga_žanr(
 create table Ponuditelj(
 	korisničkoIme varchar(32) not null primary key
 	, lozinka varchar(32) not null
-	, naziv varchar(64) not null
+	, naziv varchar(64) not null unique
 	, adresa varchar not null
 	, epošta varchar not null --check 
 	, telefon varchar not null 
 	, lokacija point 
+	, tip varchar not null
 );
 
 create table ČekaOdobrenje(
@@ -43,22 +44,34 @@ create table ČekaOdobrenje(
 	, status varchar
 );
 
-create table Izdavač(
-	korisničkoIme varchar(32) primary key references Ponuditelj(korisničkoIme)
-);
-
-create table Antikvarijat(
-	korisničkoIme varchar(32) primary key references Ponuditelj(korisničkoIme)
-);
-
-create table Preprodavač(
-	korisničkoIme varchar(32) primary key references Ponuditelj(korisničkoIme)
-);
-
 create table nudi(
-	korisničkoIme varchar(32) not null references Ponuditelj(korisničkoIme)
+	--korisničkoIme varchar(32) not null references Ponuditelj(korisničkoIme)
+	naziv varchar(64) not null references Ponuditelj(naziv)
 	, ISBN char(13) not null references Knjiga(ISBN)
 	, broj int not null check (broj >= 0)
 	, cijena decimal(6, 2) not null check (cijena >= 0)
-	, primary key(ISBN, korisničkoIme)
+	, primary key(ISBN, naziv)
 );
+
+----
+
+--revoke connect on database StoZelisCitati from public;
+revoke all on schema public from public;
+/*
+create role administrator with createuser password '5959kk';
+grant connect, create on database StoZelisCitati to administrator;
+grant all on schema public to administrator;
+grant all privileges on all tables in schema public to administrator;
+*/
+
+create user administrator with superuser password '5959kk';
+
+create role ponuditelj;
+grant usage on schema public to punuditelj;
+
+grant select on table Knjiga to public;
+grant select on table Knjiga_autori to public;
+grant select on table Knjiga_dobavljivost to public;
+grant select on table Knjiga_žanr to public;
+grant select on table nudi to public;
+grant select (naziv, adresa, epošta, telefon, lokacija, tip) on Ponuditelj to public;
