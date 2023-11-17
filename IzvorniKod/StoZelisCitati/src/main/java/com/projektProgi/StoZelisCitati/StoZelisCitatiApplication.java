e06b00a6583af87ecf89826cf10aa986ee4af8a0
Zela0912 <141058666+Zela0912@users.noreply.github.com>
2023-11-17 17:24:36 +0100

    Add files via upload

---
+++ IzvorniKod/StoZelisCitati/src/main/java/com/projektProgi/StoZelisCitati/StoZelisCitatiApplication.java
@@ -0,0 +1,18 @@
+package com.projektProgi.StoZelisCitati;
+
+import org.springframework.boot.CommandLineRunner;
+import    org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoZelisCitatiApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoZelisCitatiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
