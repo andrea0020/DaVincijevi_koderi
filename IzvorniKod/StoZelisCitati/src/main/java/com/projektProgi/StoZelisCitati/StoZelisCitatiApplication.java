55083b768f0d8070266016829f2d6c336eccea15
davormarenic <148472774+davormarenic@users.noreply.github.com>
2023-11-19 17:08:05 +0100

    Update StoZelisCitatiApplication.java

--- IzvorniKod/StoZelisCitati/src/main/java/com/projektProgi/StoZelisCitati/StoZelisCitatiApplication.java
+++ IzvorniKod/StoZelisCitati/src/main/java/com/projektProgi/StoZelisCitati/StoZelisCitatiApplication.java
@@ -3,6 +3,11 @@ package com.projektProgi.StoZelisCitati;
 import org.springframework.boot.CommandLineRunner;
 import    org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
 
 @SpringBootApplication
 public class StoZelisCitatiApplication implements CommandLineRunner {
