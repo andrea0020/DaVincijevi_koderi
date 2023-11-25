4fb5e4ea7683cd3cbfceaef114d390719a9ecb35
Zela0912 <141058666+Zela0912@users.noreply.github.com>
2023-11-25 03:04:41 +0100

    Delete StoZelisCitati directory

--- StoZelisCitati/src/main/java/com/projektProgi/StoZelisCitati/repository/KorisnikRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projektProgi.StoZelisCitati.repository;
-
-import com.projektProgi.StoZelisCitati.models.Korisnik;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KorisnikRepository extends JpaRepository<Korisnik,Long> {
-}
