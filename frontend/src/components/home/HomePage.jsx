ca8cd2deb0c4020bdaf4a17c3668c0573f02b258
SimaVuletic <sv54222@fer.hr>
2024-01-11 17:36:09 +0100

    1

--- frontend/src/components/home/HomePage.jsx
+++ frontend/src/components/home/HomePage.jsx
@@ -1,30 +1,12 @@
-//import { Link } from 'react-router-dom'
-import { Navbar } from '../navbar/Navbar'
-
-//export function HomePage() {
- // return (
-   // <div className='container'>
-     //   <Navbar />
-    //</div>
-  //)
-//}
-
 import React from 'react';
-//import { Navbar } from '../navbar/Navbar';
-import { Navbar as MyNavbar } from '../navbar/Navbar';
-import SearchButton from '../SearchButton/SearchButton';
-import '../SearchButton/SearchButton.css'; // Import the CSS file
+import { Navbar } from '../navbar/Navbar'
 
 export function HomePage() {
   return (
     <div className='container'>
-       <MyNavbar /> {/* Use the alias MyNavbar */}
-      <h1>Welcome to the Home Page</h1>
-      <div className="SearchButton"> {/* Apply the correct class name here */}
-        <SearchButton />
-      </div>
-      {/* Additional content for your home page */}
+        <Navbar />
     </div>
-  );
+  )
 }
+
 
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom";
+import SearchButton from '../SearchButton/SearchButton';
+import '../SearchButton/SearchButton.css';
 
 export function Navbar() {
    return (
       <div className="navbar">
          <img src="/images/logo.png" className="logo" alt="" style={{ marginLeft: '25px' }}/>
-      <nav>
+        <div>
+            <SearchButton/>
+        </div>
+      <nav> 
+        
             <ul>
+                
                 <li><Link to="/">HOME</Link></li>
                 <li><Link to="/login">LOGIN</Link></li>
                 <li><Link to="/register">REGISTER</Link></li>
             </ul>
         </nav>
+       
         <button className="hamburger " >
             <div className="bar"></div>
         </button>
